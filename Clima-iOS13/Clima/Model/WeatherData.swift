9d2108aa4b66fdb39a7bf9258952c03f1100d0c8
ryu-yeon <rytak108@gmail.com>
2022-04-22 17:59:28 +0900

    create Clima

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by 유연탁 on 2022/04/21.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+ }
