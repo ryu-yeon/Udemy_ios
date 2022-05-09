c2c1fbcd28b22e85755be9d1d3db8345315e743d
ryu-yeon <rytak108@gmail.com>
2022-05-09 16:02:45 +0900

    create I AM Rich-SwiftUI

---
+++ I Am Rich-swiftUI/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by 유연탁 on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
