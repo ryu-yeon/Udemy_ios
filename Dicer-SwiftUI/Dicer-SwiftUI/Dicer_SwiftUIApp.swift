9ec736c5b6dec35f073837328b3e12d85cc2e11a
ryu-yeon <rytak108@gmail.com>
2022-05-09 16:04:20 +0900

    create Dicer-SwiftUI

---
+++ Dicer-SwiftUI/Dicer-SwiftUI/Dicer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicer_SwiftUIApp.swift
+//  Dicer-SwiftUI
+//
+//  Created by 유연탁 on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct Dicer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
